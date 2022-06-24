b13e5980e9f0afa2a4877aa7650fb5e9eb9e1936
aljailnai1975 <107601734+aljailnai1975@users.noreply.github.com>
2022-06-24 15:19:23 +0400

    full_stack added

---
+++ Lecture40_JS/js/script2.js
@@ -0,0 +1,12 @@
+//-----------------------script2.js
+(function (window) {
+  var johnGreeter = {};
+  johnGreeter.name = "John";
+  var greeting = "Hi ";
+  johnGreeter.sayHi = function () {
+    console.log(greeting + johnGreeter.name);
+  }
+
+  window.johnGreeter = johnGreeter;
+
+})(window);
